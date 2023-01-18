83b03275c8889992433b786b2df136ceb85d11b6
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2023-01-18 15:10:57 +0900

    HTML5のinputタグの新機能を使う

---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/TextBoxHtml5Input.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TextBoxHtml5Input.aspx.cs" Inherits="ASP.NET_Training.Training.WebServerControl.TextBoxHtml5Input" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--
+                HTML5では、inputタグのtype属性に幾つかの新しい値が追加され、日付入力、メールアドレス入力、、電話番号などのサポートされるようになった
+
+                Color:          色
+                Date:           日付
+                DateTime:       UTC(協定世界時)による日時
+                DateTimeLocal:  UTCによらないローカル日時
+                Email:          メールアドレス
+                Month:          月
+                Number:         数値
+                Phone:          電話番号
+                Range:          数値範囲
+                Search:         検査文字列
+                Time:           時間
+                Url:            URL
+                Week:           週
+                --%>
+            Color:
+            <asp:TextBox ID="TextBox1" runat="server" TextMode="Color"></asp:TextBox>
+            <br/>
+            Date:
+            <asp:TextBox ID="TextBox2" runat="server" TextMode="Date"></asp:TextBox>
+            <br/>
+            DateTime:
+            <asp:TextBox ID="TextBox3" runat="server" TextMode="DateTime"></asp:TextBox>
+            <br/>
+            DateTimeLocal:
+            <asp:TextBox ID="TextBox4" runat="server" TextMode="DateTimeLocal"></asp:TextBox>
+            <br/>
+            Email:
+            <asp:TextBox ID="TextBox5" runat="server" TextMode="Email"></asp:TextBox>
+            <br/>
+            Month:
+            <asp:TextBox ID="TextBox6" runat="server" TextMode="Month"></asp:TextBox>
+            <br/>
+            Number:
+            <asp:TextBox ID="TextBox7" runat="server" TextMode="Number"></asp:TextBox>
+            <br/>
+            Phone:
+            <asp:TextBox ID="TextBox8" runat="server" TextMode="Phone"></asp:TextBox>
+            <br/>
+            Range:
+            <asp:TextBox ID="TextBox9" runat="server" TextMode="Range"></asp:TextBox>
+            <br/>
+            Search:
+            <asp:TextBox ID="TextBox10" runat="server" TextMode="Search"></asp:TextBox>
+            <br/>
+            Time:
+            <asp:TextBox ID="TextBox11" runat="server" TextMode="Time"></asp:TextBox>
+            <br/>
+            Url:
+            <asp:TextBox ID="TextBox12" runat="server" TextMode="Url"></asp:TextBox>
+            <br/>
+            Week:
+            <asp:TextBox ID="TextBox13" runat="server" TextMode="Week"></asp:TextBox>
+            <br/>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/TextBoxHtml5Input.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、正しくない動作の原因になる可能性があり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace ASP.NET_Training.Training.WebServerControl
+{
+
+
+    public partial class TextBoxHtml5Input
+    {
+
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// TextBox1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// TextBox2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// TextBox3 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// TextBox4 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// TextBox5 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// TextBox6 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// TextBox7 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// TextBox8 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// TextBox9 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+
+        /// <summary>
+        /// TextBox10 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox10;
+
+        /// <summary>
+        /// TextBox11 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox11;
+
+        /// <summary>
+        /// TextBox12 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox12;
+
+        /// <summary>
+        /// TextBox13 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox13;
+    }
+}
