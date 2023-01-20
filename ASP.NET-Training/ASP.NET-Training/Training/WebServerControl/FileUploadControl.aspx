5c2d2cddb7f3a062227ce5808a8f59e03cde7a7d
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2023-01-20 17:56:21 +0900

    ファイルアップロードを使用する

---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/FileUploadControl.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUploadControl.aspx.cs" Inherits="ASP.NET_Training.Training.WebServerControl.FileUploadControl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%-- 
+                FileUploadコントロールはWebブラウザーからファイルをアップロードするためのコントロールである
+                FileUploadコントロールには、共通プロパティ以外はデザイン時に使用可能なプロパティはない
+                また、HTMLのファイルアップロード機能の制限により、予めアップロードするファイルの初期値を指定することもできない
+                また、FileUploadコントロールは自動ポストバックに対応していないため、ボタン系コントロールなど、ポストバックを行うコントロールを別途で配置する必要がある
+
+                ファイルのアップロードの際のファイルサイズ上限は、Webアプリケーションの構成ファイルであるWeb.configファイルで設定できる
+                デフォルトは4MBが上限となっている
+                --%>
+
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br/>
+            <br/>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br/>
+            <br/>
+            <asp:Button ID="Button1" runat="server" Text="アップロード" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/FileUploadControl.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_Training.Training.WebServerControl
+{
+    public partial class FileUploadControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+            ポストバック後にFileUploadコントロールで使用可能なプロパティ
+                
+                HasFile:        ファイルがアップロードされたかどうか
+                PostedFile:     アップロードされたファイル
+
+            PostedFileプロパティでは、アップロードされたファイルを表すHttpPostedFileというクラスのインスタンスを取得できる
+
+                FileNameプロパティ:          ファイルの名前。Internet Explorerの場合はフルパス
+                ContentLengthプロパティ:     ファイルのサイズ
+                ContentTypeプロパティ:       ファイルの種類
+                InputStreamプロパティ:       ファイルの内容
+                SaveAsメソッド:              アップロードされたファイルに名前を付けて保存する
+             */
+
+            //ファイルがアップロードされていれば
+            if(FileUpload1.HasFile == true)
+            {
+                //アップロードされたファイルを取得する
+                var file = FileUpload1.PostedFile;
+                Label1.Text = "アップロードされたファイルの情報<br/>";
+                Label1.Text += string.Format("ファイル名：{0}<br/>",file.FileName);
+                Label1.Text += string.Format("ファイルサイズ：{0}バイト<br/>", file.ContentLength);
+                Label1.Text += string.Format("コンテントタイプ：{0}<br/>", file.ContentType);
+
+                //アップロードされたファイルを一時フォルダに保存する
+                //System.IO.Path.GetTempPath()は一時フォルダを取得するメソッド
+                //System.IO.Path.GetFileName()はフルパスからファイル名を取得するメソッド
+                file.SaveAs(
+                    System.IO.Path.GetTempPath() +
+                    System.IO.Path.GetFileName(file.FileName));
+            }
+
+        }
+    }
+}
