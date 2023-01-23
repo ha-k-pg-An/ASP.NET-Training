caaee20de3b9c310b2f9e776334200b95e72a050
ANHEI <anhei19960725@yahoo.co.jp>
2023-01-23 20:21:30 +0900

    複数ファイルをアップロードする

---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/MultipleFileUpload.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MultipleFileUpload.aspx.cs" Inherits="ASP.NET_Training.Training.WebServerControl.MultipleFileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%-- 
+                AllowMultiple: 複数ファイルアップロードを受け付けるかどうか                
+                --%>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br/><br/>
+            <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="True" />
+            <br/><br/>
+            <asp:Button ID="Button1" runat="server" Text="アップロード" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Training/ASP.NET-Training/Training/WebServerControl/MultipleFileUpload.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_Training.Training.WebServerControl
+{
+    public partial class MultipleFileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+                HasFiles:       ファイルがアップロードされているかどうか
+                PostedFiles:    アップロードされたファイルのコレクション
+             */
+
+            //ファイルがアップロードされていれば
+            if(FileUpload1.HasFiles)
+            {
+                //アップロードされたファイルをコレクションから1つずつ取得する
+                foreach(var file in FileUpload1.PostedFiles)
+                {
+                    Label1.Text += "アップロードされたファイルの情報<br/>";
+                    Label1.Text += String.Format("ファイル名：{0}<br/>", file.FileName);
+                    Label1.Text += String.Format("ファイルサイズ：{0}<br/>", file.ContentLength);
+                    Label1.Text += String.Format("ファイルタイプ：{0}<br/>", file.ContentType);
+
+                    //アップロードされたファイルを一時ファイルに保存する
+                    file.SaveAs(
+                        System.IO.Path.GetTempPath() +
+                        System.IO.Path.GetFileName(file.FileName));
+                }
+            }
+        }
+    }
+}
